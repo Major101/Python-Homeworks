76024a4ac403787d03d1db26def73bcc5228dffe
Jamshid <jumabayevjamshid7@gmail.com>
2022-11-21 10:47:39 +0500

    4-vazifa

--- Exceptions/jamshid.jumabaev/hello.py
+++ Exceptions/jamshid.jumabaev/hello.py
@@ -1,1 +1,23 @@
-print('Hello')
+
+n=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+while True:
+    
+        n = input('data:') 
+        total = n[n]
+
+        
+        print(f'${total:.2f}')
+
